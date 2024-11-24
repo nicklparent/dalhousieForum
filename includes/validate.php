368b94ddfc71b4ec272a0c5366cdd3db382e6c48
Nicholas Parent <nick.lparent@outlook.com>
2024-11-24 01:11:18 -0400

    validater class made

--- includes/validate.php
+++ includes/validate.php
@@ -1,3 +1,22 @@
 <?php
 // Validate functionality
+class validater {
+    private $valid;
+    public function __construct(){
+        $this->valid = false;
+    }
+
+    public function validate($data){
+
+    }
+
+    private function injectionCheck($data){
+
+    }
+
+    public function getValid(){
+        return $this->valid;
+    }
+
+}
 ?>
--- messages.php
+++ messages.php
@@ -2,6 +2,7 @@
 // Messages functionality -- Required feature
 session_start();
 require_once "includes/db_connect.php";
+require_once "includes/validate.php";
 
 if (isset($_SESSION["loggedIn"])) {
     header("Location: index.php", true, 302);
